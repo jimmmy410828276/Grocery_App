6122a5e659a942f5df262af94b02f16237be2719
jimmmy410828276 <s10828276@gm.pu.edu.tw>
2022-10-21 18:56:52 +0800

    Initial commit

---
+++ app/src/main/java/com/tingtingman/groceryapp/Constants.java
@@ -0,0 +1,40 @@
+package com.tingtingman.groceryapp;
+
+public class Constants {
+
+    public static final String FCM_KEY = "AAAA3qeO8Hk:APA91bF3m36lSaoBgqGQg9ZNwvhG-p0c9lEIhe4rtSAOcrmT0v7DpGtvcDeOelBAEHDY6TLHN8co3ZleZDYgSdQC6zi4gPqUd3rFS41dWRw-nsMAxjaAuVFoh2CsuyztOacrzilJ4AjQ";
+    public static final String FCM_TOPIC = "PUSH_NOTIFICATIONS";
+
+    //product categories
+    public static final String[] productCategories = {
+            "Beverages",
+            "Beauty & Personal Care",
+            "Baby Kids",
+            "Biscuits Snacks & Chocolates",
+            "Breakfast & Dairy",
+            "Cooking Needs",
+            "Frozen Food",
+            "Fruits",
+            "Pet Cate",
+            "Pharmacy",
+            "Vegetables",
+            "Others"
+    };
+
+    public static final String[] productCategories1 = {
+            "All",
+            "Beverages",
+            "Beauty & Personal Care",
+            "Baby Kids",
+            "Biscuits Snacks & Chocolates",
+            "Breakfast & Dairy",
+            "Cooking Needs",
+            "Frozen Food",
+            "Fruits",
+            "Pet Cate",
+            "Pharmacy",
+            "Vegetables",
+            "Others"
+    };
+
+}
